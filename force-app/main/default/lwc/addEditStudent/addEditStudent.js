9e3f17c33aed79a4126aa7799963b6fe5da48d84
Rinku Bangale <bangalehemant@gmail.com>
2022-11-10 20:49:08 +0530

    New Components added

---
+++ force-app/main/default/lwc/addEditStudent/addEditStudent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+
+export default class AddEditStudent extends LightningElement {
+  headTitle = "Add Student";
+  addStudentRecord() {
+    console.log("Wait..., No hurry I will work some day!");
+  }
+}
---
+++ force-app/main/default/lwc/addEditSubject/addEditSubject.js
@@ -0,0 +1,5 @@
+import { LightningElement } from "lwc";
+
+export default class AddEditSubject extends LightningElement {
+  headTitle = "Add Subject";
+}
