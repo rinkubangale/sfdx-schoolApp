24e102bf5fa2c6f228c9adda2431ee8a5bc2000b
Rinku Bangale <bangalehemant@gmail.com>
2022-11-11 20:13:43 +0530

    few changes in list view components search bar added

--- force-app/main/default/lwc/screenContainer/screenContainer.js
+++ force-app/main/default/lwc/screenContainer/screenContainer.js
@@ -1,36 +1,42 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track, api } from "lwc";
 import { loadStyle } from "lightning/platformResourceLoader";
 import externalVidCSS from "@salesforce/resourceUrl/externalVidCSS";
 
 export default class ScreenContainer extends LightningElement {
+  isCssLoaded = false;
+  renderedCallback() {
+    if (this.isCssLoaded) return;
+    this.isCssLoaded = true;
+    Promise.all([loadStyle(this, externalVidCSS)])
+      .then(() => {
+        console.log("Files loaded");
+      })
+      .catch((error) => {
+        console.log(error.body.message);
+      });
+  }
 
-    renderedCallback() {
+  @track showContainer = {
+    admission: false,
+    home: true,
+    fee: false,
+    settings: false,
+    subscription: false,
+    showCheckin: false,
+    backbtn: false
+  };
 
-        Promise.all([
-            loadStyle(this, externalVidCSS)
-        ]).then(() => {
-            console.log("Files loaded");
-        })
-            .catch(error => {
-                console.log(error.body.message);
-            });
+  @track toggle = false;
 
-    }
+  handleBtnClick(e) {
+    this.showContainer = e.detail;
+  }
 
-    @track showContainer =
-        { admission: false, home: true, fee: false, settings: false, subscription: false, showCheckin: false, backbtn: false };
+  handleToggle(e) {
+    this.toggle = e.detail;
+  }
 
-    @track toggle = false;
-
-    handleBtnClick(e) {
-        this.showContainer = e.detail;
-    }
-
-    handleToggle(e) {
-        this.toggle = e.detail;
-    }
-
-    get yContainerLeft() {
-        return this.toggle ? "yContainerLeft toggle" : "yContainerLeft";
-    }
+  get yContainerLeft() {
+    return this.toggle ? "yContainerLeft toggle" : "yContainerLeft";
+  }
 }
