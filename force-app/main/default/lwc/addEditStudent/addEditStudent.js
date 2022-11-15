e47b6a051f65dfd4c3b814ed33fc999b2b0e2251
Rinku Bangale <bangalehemant@gmail.com>
2022-11-15 10:44:56 +0530

    add edit wireframe attached with manage student component

--- force-app/main/default/lwc/addEditStudent/addEditStudent.js
+++ force-app/main/default/lwc/addEditStudent/addEditStudent.js
@@ -5,4 +5,8 @@ export default class AddEditStudent extends LightningElement {
   addStudentRecord() {
     console.log("Wait..., No hurry I will work some day!");
   }
+
+  handleModalChange() {
+    this.dispatchEvent(new CustomEvent("modalchange", { detail: false }));
+  }
 }
