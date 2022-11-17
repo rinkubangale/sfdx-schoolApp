bee9c1db198e3ed1553c9ad4e377fe3a9a59543b
Rinku Bangale <bangalehemant@gmail.com>
2022-11-17 22:13:14 +0530

    css added

--- force-app/main/default/lwc/manageClassroom/manageClassroom.js
+++ force-app/main/default/lwc/manageClassroom/manageClassroom.js
@@ -1,26 +1,35 @@
-import { LightningElement,track,wire } from 'lwc';
+import { LightningElement, track, wire } from "lwc";
 import listClass from "@salesforce/apex/listClass.listClass";
 
 export default class ManageClassroom extends LightningElement {
-    @track data;
-    @track sectionlist = [{"Name":"A","Class__c":"a0H6D000004KgdiUAC","LastModifiedById":"0056D000005sejwQAA","Id":"a0U6D000004pZynUAE","Class__r":{"Name":"8","Id":"a0H6D000004KgdiUAC"},"LastModifiedBy":{"Name":"Rinku Bangale","Id":"0056D000005sejwQAA"}},{"Name":"B","Class__c":"a0H6D000004KgdiUAC","LastModifiedById":"0056D000005sejwQAA","Id":"a0U6D000004pZyoUAE","Class__r":{"Name":"8","Id":"a0H6D000004KgdiUAC"},"LastModifiedBy":{"Name":"Rinku Bangale","Id":"0056D000005sejwQAA"}}];
-    @wire(listClass)
-    wiredClasses({ error, data }) {
-      if (data) {
-        this.data = data.map((item) => {
-          return {
-            Id: item.Id,
-            Name: item.Name,
-            Class__c: item.Class__r.Name,
-            // Sections__r: item.Sections__r.Name,
-            // Department__c: item.Department__r.Name,
-            LastModifiedBy: item.LastModifiedBy.Name
-          };
-        });
-        this.error = undefined;
-      } else if (error) {
-        this.error = error;
-        this.data = undefined;
-      }
-    }
+  @track modalopen;
+  @track createmodal;
+  openModal() {
+    // to open modal set isModalOpen tarck value as true
+    this.isModalOpen = true;
+  }
+  closeModal() {
+    // to close modal set isModalOpen tarck value as false
+    this.isModalOpen = false;
+  }
+  submitDetails() {
+    // to close modal set isModalOpen tarck value as false
+    //Add your code to call apex method or do some processing
+    this.isModalOpen = false;
+  }
+  handelModel() {
+    this.createmodal = !this.createmodal;
+    console.log("check" + this.createmodal);
+  }
+  handleModalChange(event) {
+    this.isModalOpen = event.detail;
+  }
+  goBack() {
+    // history.back();
+    this.dispatchEvent(
+      new CustomEvent("backbtn", {
+        detail: false
+      })
+    );
+  }
 }
