43fe31b0822164914b45101686a9d871da8a7858
Rinku Bangale <bangalehemant@gmail.com>
2022-11-30 15:01:14 +0530

    add request component added

--- force-app/main/default/lwc/featureRequest/featureRequest.js
+++ force-app/main/default/lwc/featureRequest/featureRequest.js
@@ -3,6 +3,6 @@ import { LightningElement } from "lwc";
 export default class FeatureRequest extends LightningElement {
   submitIdea = false;
   handleSubmitIdea() {
-    this.submitIdea = true;
+    this.submitIdea = !this.submitIdea;
   }
 }
--- force-app/main/default/lwc/submitIdea/submitIdea.js
+++ force-app/main/default/lwc/submitIdea/submitIdea.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class SubmitIdea extends LightningElement {}
+export default class SubmitIdea extends LightningElement {
+  handleModalChange() {
+    this.dispatchEvent(new CustomEvent("back", { detail: false }));
+  }
+}
