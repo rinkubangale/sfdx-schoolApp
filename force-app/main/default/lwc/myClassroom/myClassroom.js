e7f5a94687df55ed7680345ee73d728afa90eebe
Rinku Bangale <bangalehemant@gmail.com>
2022-12-09 19:06:23 +0530

    added css

--- force-app/main/default/lwc/myClassroom/myClassroom.js
+++ force-app/main/default/lwc/myClassroom/myClassroom.js
@@ -1,10 +1,13 @@
 import { LightningElement, track } from "lwc";
 
 export default class MyClassroom extends LightningElement {
-  isTimetable = true;
-  @track columns = [];
+  @track isTimetable = false;
+  @track columns = [0, 0, 0, 0, 0];
   handleNoColumn(e) {
     this.columns = new Array(+e.target.value).fill(0);
     console.log(this.columns, e.target.value);
+  }
+  showTimetable() {
+    this.isTimetable = true;
   }
 }
