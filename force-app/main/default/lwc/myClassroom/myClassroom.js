22c70f253c0f9951dbcf6758e920d9da3a2166b9
Rinku Bangale <bangalehemant@gmail.com>
2022-12-09 18:15:11 +0530

    calender view added in teacher flow

--- force-app/main/default/lwc/myClassroom/myClassroom.js
+++ force-app/main/default/lwc/myClassroom/myClassroom.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class MyClassroom extends LightningElement {}
+export default class MyClassroom extends LightningElement {
+  isTimetable = true;
+  @track columns = [];
+  handleNoColumn(e) {
+    this.columns = new Array(+e.target.value).fill(0);
+    console.log(this.columns, e.target.value);
+  }
+}
